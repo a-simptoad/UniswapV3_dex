0a991059de4f6c3b6a0a5594ff7590dd954c858b
a-simptoad <aryanagarwal727@gmail.com>
2025-10-06 22:15:46 +0000

    improves pool contract

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Tick} from "./lib/Tick.sol";
-import {Position} from "./lib/Position.sol";
+import {Tick} from "src/libs/Tick.sol";
+import {Position} from "src/libs/Position.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IUniswapV3MintCallback} from "src/interfaces/IUniswapV3MintCallback.sol";
 
 contract UniswapV3Pool {
     // Errors
@@ -14,12 +16,14 @@ contract UniswapV3Pool {
     using Position for mapping(bytes32 => Position.Info);
     using Position for Position.Info;
 
+    // constant and immutable variables are stored in contract's bytecode and hence takes less gas fees to read
     int24 internal constant MIN_TICK = -887272;
     int24 internal constant MAX_TICK = -MIN_TICK;
 
     address public immutable token0;
     address public immutable token1;
 
+    // First variable to be stored in contract storage and hence makes it efficient to navigate price ultimately reducing gas fees.
     struct Slot0 {
         // current sqrt(P) Q64.96
         uint160 sqrtPriceX96;
@@ -36,7 +40,15 @@ contract UniswapV3Pool {
     mapping(int24 => Tick.Info) public ticks;
     mapping(bytes32 => Position.Info) public positions;
 
-    event Mint(address sender, address owner, int24 lowerTick, int24 upperTick, uint128 amount, uint256 amount0, uint256 amount1);
+    event Mint(
+        address caller,
+        address owner,
+        int24 lowerTick,
+        int24 upperTick,
+        uint128 amount,
+        uint256 amount0,
+        uint256 amount1
+    );
 
     constructor(address _token0, address _token1, uint160 sqrtPriceX96, int24 _tick) {
         token0 = _token0;
@@ -47,12 +59,14 @@ contract UniswapV3Pool {
 
     /**
      * @notice Mint function is called to provide liquidty to the pool contract
-     * @param owner Owner's address -> To track the owner of the liquidity
+     * @param owner Owner's address -> To track the owner of the liquidity provider
      * @param lowerTick Lower bound of the price range
      * @param upperTick Upper bound of the price range
      * @param amount Amount of liquidty owner provides
-     * @return amount0
-     * @return amount1
+     * 
+     * @return amount0 Amount of token0 that LP provides
+     * @return amount1 Amount of token1 that LP provides
+     * 
      * @dev Here, User specifies Liquidty and not actual token amounts as input; The contract only implements core logic
      * and hence later on a helper contract will convert token amounts to liquidity before calling this function.
      */
@@ -69,40 +83,35 @@ contract UniswapV3Pool {
         // Initializes ticks at the price ranges
         ticks.update(lowerTick, amount);
         ticks.update(upperTick, amount);
-        
+
         // Similarily initialized a position and updates the liquidity between the ticks (defines price range)
-        Position.Info storage position = positions.get(
-            owner,
-            lowerTick,
-            upperTick
-        );
+        Position.Info storage position = positions.get(owner, lowerTick, upperTick);
         position.update(amount);
 
         // HARD-CODED VALUES OF INPUT AMOUNTS FROM THE USER
-        amount0 = 0.998976618347425280 ether;
+        amount0 = 0.99897661834742528 ether;
         amount1 = 5000 ether;
 
         liquidity += uint128(amount);
 
         uint256 balance0Before;
         uint256 balance1Before;
-        if(amount0 > 0) balance0Before = balance0();
-        if(amount1 > 0) balance1Before = balance1();
-        IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(
-            amount0,
-            amount1
-        );
-        if(amount0 > 0 && balance0Before + amount0 > balance0()) revert Pool__InsufficientInputAmount();
-        if(amount1 > 0 && balance1Before + amount1 > balance1()) revert Pool__InsufficientInputAmount();
+        if (amount0 > 0) balance0Before = balance0();
+        if (amount1 > 0) balance1Before = balance1();
+        /// @notice We call the uniswapV3MintCallback method on the caller (Router Contract)
+        /// @dev Expected that the caller is a contract and implements the uniswapV3MintCallback method and transfers tokens to this Pool contract
+        IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(amount0, amount1);
+        if (amount0 > 0 && balance0Before + amount0 > balance0()) revert Pool__InsufficientInputAmount();
+        if (amount1 > 0 && balance1Before + amount1 > balance1()) revert Pool__InsufficientInputAmount();
 
         emit Mint(msg.sender, owner, lowerTick, upperTick, amount, amount0, amount1);
     }
 
-    function balance0() internal returns (uint256 balance){
+    function balance0() internal view returns (uint256 balance) {
         balance = IERC20(token0).balanceOf(address(this));
     }
 
-    function balance1() internal returns (uint256 balance){
+    function balance1() internal view returns (uint256 balance) {
         balance = IERC20(token1).balanceOf(address(this));
     }
 }
---
+++ test/UniswapV3Pool.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {UniswapV3Pool} from "src/UniswapV3Pool.sol";
+import {ERC20Mintable} from "test/ERC20Mintable.sol";
+import {IUniswapV3MintCallback} from "src/interfaces/IUniswapV3MintCallback.sol";
+
+contract UniswapV3PoolTest is Test, IUniswapV3MintCallback{
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV3Pool pool;
+
+    struct TestCaseParams {
+        uint256 wethBalance;
+        uint256 usdcBalance;
+        int24 currentTick;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 liquidity;
+        uint160 currentSqrtP;
+        bool shouldTransferInCallback;
+        bool mintLiquidity;
+    }
+    bool shouldTransferInCallback;
+
+    address owner = makeAddr("owner");
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
+
+    function testMintSuccess() public {
+        TestCaseParams memory params = TestCaseParams({
+            wethBalance: 1 ether,
+            usdcBalance: 5000 ether,
+            currentTick: 85176,
+            lowerTick: 84222,
+            upperTick: 86129,
+            liquidity: 1517882343751509868544,
+            currentSqrtP: 5602277097478614198912276234240,
+            shouldTransferInCallback: true,
+            mintLiquidity: true
+        });
+
+        (uint256 poolBalance0, uint256 poolBalance1) = setupTestCase(params);
+        
+        uint256 expectedAmount0 = 0.99897661834742528 ether;
+        uint256 expectedAmount1 = 5000 ether;
+
+        assertEq(poolBalance0, expectedAmount0, "Incorrect token0 deposited Amount");
+        assertEq(poolBalance1, expectedAmount1, "Incorrect token1 deposited Amount");
+    }
+
+    function setupTestCase(TestCaseParams memory params) internal returns (uint256 poolBalance0, uint256 poolBalance1) {
+        token0.mint(address(this), params.wethBalance);
+        token1.mint(address(this), params.usdcBalance);
+
+        pool = new UniswapV3Pool(address(token0), address(token1), params.currentSqrtP, params.currentTick);
+
+        if(params.mintLiquidity) {
+            (poolBalance0, poolBalance1) = pool.mint(address(this), params.lowerTick, params.upperTick, params.liquidity);
+        }
+
+        shouldTransferInCallback = params.shouldTransferInCallback;
+    }
+
+    function uniswapV3MintCallback(uint256 amount0, uint256 amount1) external override{
+        if(shouldTransferInCallback) {
+            console.log("This callback function is called");
+            token0.transfer(msg.sender, amount0);
+            token1.transfer(msg.sender, amount1);
+        }
+    }
+}
